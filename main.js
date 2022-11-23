216717897305492565355fec15ded5c3b6261981
Milos1988 <vojvoda1988@gmail.com>
2022-11-23 13:37:50 +0100

    msg

---
+++ main.js
@@ -0,0 +1,48 @@
+let wordsDiv = document.querySelector(".words");
+let btn = document.querySelector(".btn")
+let fault = 10;
+let text = document.querySelector(".text")
+let attempts = document.querySelector(".attempts")
+let input = document.querySelector(".inp")
+
+let words = ["odlican", "prinesi", "aspekt", "dodaj", "obnoviti", "tuziti", "osoblje", "intrige"];
+
+let random = Math.floor(Math.random() * words.length);
+
+let randomWords = words[random]
+console.log(randomWords)
+
+for (const letter of randomWords) {
+    let span = document.createElement("span")
+    span.classList.add("span")
+    wordsDiv.appendChild(span)
+
+}
+let span = document.querySelectorAll(".span")
+attempts.innerHTML = `Reamining number of attempts: ${fault}`
+
+btn.addEventListener("click", () => {
+
+    let seletLetter = input.value;
+
+    for (let i = 0; i < randomWords.length; i++) {
+        if (randomWords[i] === seletLetter) {
+            console.log("object");
+            span[i].innerHTML = seletLetter
+        }
+
+
+    }
+    let arrawWords = [...randomWords]
+    if (!arrawWords.includes(seletLetter)) {
+        fault--
+        if (fault < 1) {
+            text.innerHTML = "GAME OVER"
+            btn.disabled = true;
+        }
+    }
+
+    attempts.innerHTML = `Reamining number of attempts: ${fault}`
+    input.value = ''
+}
+)
