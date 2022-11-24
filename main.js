c4c12084de508ca8191ce1db92d7b47c1fca9aa0
Milos1988 <vojvoda1988@gmail.com>
2022-11-24 06:26:20 +0100

    finish finish

--- main.js
+++ main.js
@@ -5,22 +5,24 @@ let text = document.querySelector(".gameOver")
 let attempts = document.querySelector(".attempts")
 let input = document.querySelector(".inp")
 let newGame = document.querySelector(".newGame")
+let startGame = document.querySelector(".startGame")
 let msg = document.querySelector(".msg")
-
-
+let win = document.querySelector(".win")
 let words = ["odlican", "prinesi", "aspekt", "dodaj", "obnoviti", "tuziti", "osoblje", "intrige"];
 
 let random = Math.floor(Math.random() * words.length);
-
 let randomWords = words[random]
+
 console.log(randomWords)
+
+input.focus()
 
 for (const letter of randomWords) {
     let span = document.createElement("span")
     span.classList.add("span")
     wordsDiv.appendChild(span)
-
 }
+
 let span = document.querySelectorAll(".span")
 attempts.innerHTML = `Reamining number of attempts: ${fault}`
 
@@ -30,26 +32,44 @@ btn.addEventListener("click", () => {
 
     for (let i = 0; i < randomWords.length; i++) {
         if (randomWords[i] === seletLetter) {
-            console.log("object");
             span[i].innerHTML = seletLetter
         }
-
-
     }
+let niz=[]
+    span.forEach(e=>{
+        if(e.innerText!==""){
+            niz.push(e)
+        }
+    })
+    if(niz.length===randomWords.length){
+
+        win.style.display="block"
+        btn.disabled = true;
+            input.disabled = true;
+    }
+
     let arrawWords = [...randomWords]
-    if (!arrawWords.includes(seletLetter)) {
+
+    if (!arrawWords.includes(seletLetter) && seletLetter !=='') {
         fault--
+        
         if (fault < 1) {
             msg.style.display = "block"
             btn.disabled = true;
+            input.disabled = true;
         }
     }
 
     attempts.innerHTML = `Reamining number of attempts: ${fault}`
     input.value = ''
+    input.focus()
 }
 )
 newGame.addEventListener("click", () => {
+    location.reload()
+}
+)
+startGame.addEventListener("click", () => {
     location.reload()
 }
 )
