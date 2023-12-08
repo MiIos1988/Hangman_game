0b4163794dd874d340af9067a0d0949ece702460
Milos1988 <vojvoda1988@gmail.com>
2023-12-09 00:19:38 +0100

    Corrected code

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,40 @@
-let wordsDiv = document.querySelector(".words");
-let btn = document.querySelector(".btn");
+const wordsDiv = document.querySelector(".words")!;
+const btn: HTMLButtonElement = document.querySelector(".btn")!;
 let fault = 10;
-let text = document.querySelector(".gameOver");
-let attempts = document.querySelector(".attempts");
-let input = document.querySelector(".inp");
-let newGame = document.querySelector(".newGame");
-let startGame = document.querySelector(".startGame");
-let msg = document.querySelector(".msg");
-let win = document.querySelector(".win");
-let words = ["elephant", "computer", "bicycle", "sunshine", "library", "butterfly", "rainbow", "adventure", "champion", "delicious", "guitar", "kangaroo", "mountain", "jigsaw", "excitement"];
-let random = Math.floor(Math.random() * words.length);
-let randomWords = words[random];
+const text = document.querySelector(".gameOver");
+const attempts = document.querySelector(".attempts")!;
+const input: HTMLInputElement = document.querySelector(".inp")!;
+const newGame = document.querySelector(".newGame");
+const startGame = document.querySelector(".startGame");
+const msg = document.querySelector(".msg");
+const win = document.querySelector(".win");
+const words = ["elephant", "computer", "bicycle", "sunshine", "library", "butterfly", "rainbow", "adventure", "champion", "delicious", "guitar", "kangaroo", "mountain", "jigsaw", "excitement"];
+const random = Math.floor(Math.random() * words.length);
+const randomWords = words[random];
 
 console.log(randomWords);
 
 input.focus();
 
 for (const letter of randomWords) {
-    let span = document.createElement("span");
+    const span = document.createElement("span");
     span.classList.add("span");
     wordsDiv.appendChild(span);
 }
-
-let span = document.querySelectorAll(".span");
+const span = document.querySelectorAll(".span");
 attempts.innerHTML = `Remaining number of attempts: ${fault}`;
 
 btn.addEventListener("click", () => {
-
-    let selectLetter = input.value;
+    const selectLetter = input.value;
 
     for (let i = 0; i < randomWords.length; i++) {
         if (randomWords[i] === selectLetter) {
             span[i].innerHTML = selectLetter;
         }
     }
-let niz=[]
-    span.forEach(e=>{
+let niz=[];
+
+    span.forEach(e => {
         if(e.innerText!==""){
             niz.push(e);
         }
@@ -44,7 +43,7 @@ let niz=[]
 
         win.style.display="block";
         btn.disabled = true;
-            input.disabled = true;
+        input.disabled = true;
     }
 
     let arrowWords = [...randomWords];
