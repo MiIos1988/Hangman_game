1590ef6d569ecac350bd2649729fb79ca91db999
Milos1988 <vojvoda1988@gmail.com>
2023-12-12 00:24:51 +0100

    Converted to typescript

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 type DivField = HTMLDivElement | null;
 
-const wordsDiv = document.querySelector(".words")!;
-const btn: HTMLButtonElement = document.querySelector(".btn")!;
+const wordsDiv = document.querySelector(".words");
+const btn: HTMLButtonElement | null = document.querySelector(".btn");
 let fault = 10;
 const displayWord = document.querySelector(".displayWord");
 const text = document.querySelector(".gameOver");
-const attempts = document.querySelector(".attempts")!;
-const input: HTMLInputElement = document.querySelector(".inp")!;
+const attempts = document.querySelector(".attempts");
+const input: HTMLInputElement | null = document.querySelector(".inp");
 const newGame = document.querySelector(".newGame");
 const startGame = document.querySelector(".startGame");
 const msg: DivField = document.querySelector(".msg");
@@ -31,60 +31,65 @@ const words = [
 const random = Math.floor(Math.random() * words.length);
 const randomWords = words[random];
 
-input.focus();
+input !== null && input.focus();
 
 for (const letter of randomWords) {
   const span = document.createElement("span");
   span.classList.add("span");
-  wordsDiv.appendChild(span);
+  wordsDiv !== null && wordsDiv.appendChild(span);
 }
-const span:  NodeListOf<HTMLSpanElement> = document.querySelectorAll(".span");
-attempts.innerHTML = `Remaining number of attempts: ${fault}`;
+const span: NodeListOf<HTMLSpanElement> = document.querySelectorAll(".span");
+if (attempts !== null)
+  attempts.innerHTML = `Remaining number of attempts: ${fault}`;
 
-btn.addEventListener("click", () => {
-  const selectLetter = input.value;
+if (input !== null) {
+  btn !== null &&
+    btn.addEventListener("click", () => {
+      const selectLetter = input.value;
 
-  for (let i = 0; i < randomWords.length; i++) {
-    if (randomWords[i] === selectLetter) {
-      span[i].innerHTML = selectLetter;
-    }
-  }
-  let arr = [];
-
-  span.forEach((e: HTMLSpanElement) => {
-    if (e.innerText !== "") {
-      arr.push(e);
-    }
-  });
-  if (arr.length === randomWords.length) {
-    if (win !== null) {
-      win.style.display = "block";
-      btn.disabled = true;
-      input.disabled = true;
-    }
-  }
-
-  let arrowWords = [...randomWords];
-
-  if (!arrowWords.includes(selectLetter) && selectLetter !== "") {
-    fault--;
-
-    if (fault < 1) {
-      if (msg !== null) {
-        msg.style.display = "block";
-        btn.disabled = true;
-        input.disabled = true;
-        if(displayWord !== null) {
-            displayWord.innerHTML = randomWords;
+      for (let i = 0; i < randomWords.length; i++) {
+        if (randomWords[i] === selectLetter) {
+          span[i].innerHTML = selectLetter;
         }
       }
-    }
-  }
+      let arr = [];
 
-  attempts.innerHTML = `Remaining number of attempts: ${fault}`;
-  input.value = "";
-  input.focus();
-});
+      span.forEach((e: HTMLSpanElement) => {
+        if (e.innerText !== "") {
+          arr.push(e);
+        }
+      });
+      if (arr.length === randomWords.length) {
+        if (win !== null) {
+          win.style.display = "block";
+          btn.disabled = true;
+          input.disabled = true;
+        }
+      }
+
+      let arrowWords = [...randomWords];
+
+      if (!arrowWords.includes(selectLetter) && selectLetter !== "") {
+        fault--;
+
+        if (fault < 1) {
+          if (msg !== null) {
+            msg.style.display = "block";
+            btn.disabled = true;
+            input.disabled = true;
+            if (displayWord !== null) {
+              displayWord.innerHTML = randomWords;
+            }
+          }
+        }
+      }
+
+      if (attempts !== null)
+        attempts.innerHTML = `Remaining number of attempts: ${fault}`;
+      input.value = "";
+      input.focus();
+    });
+}
 
 newGame !== null &&
   newGame.addEventListener("click", () => {
